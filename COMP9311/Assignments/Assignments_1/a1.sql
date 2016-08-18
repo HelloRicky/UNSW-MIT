7052f8f89ebb50aa649126c376bd1d516b36c3ca
HelloRicky <ricky.zheng2@gmail.com>
2016-08-18 23:32:41 +1000

    all structure added

--- COMP9311/Assignments/Assignments_1/a1.sql
+++ COMP9311/Assignments/Assignments_1/a1.sql
@@ -21,10 +21,10 @@ create domain PhoneType as
 
 create table Employee (
 	EID          serial, 
-    FirstName	 text,
-    LastName 	 text,
+    FirstName	 varchar(50) not null,
+    LastName 	 varchar(50) not null,
     Salary       integer not null check (Salary > 0),
-    TFN			 integer,
+    TFN			 integer(9) not null,
 	primary key (EID)
 );
 
@@ -36,13 +36,13 @@ create table Admin (
 
 create table Mechanic (
 	EID			serial references Employee(EID),
-	License 	text,
+	License 	char(8) not null,
 	primary key (EID)
 );
 
 create table Salesman (
 	EID			serial references Employee(EID),
-	CommRate 	text,
+	CommRate 	integer not null constraint ValidCommRate check (CommRate >= 5 and CommRate <= 20),
 	primary key (EID) 	
 );
 
@@ -50,8 +50,8 @@ create table Salesman (
 
 create table Client (
 	CID          serial,
-	Name 		 text,
-	Address  	 text,
+	Name 		 varchar(100),
+	Address  	 varchar(200),
 	Phone 		 PhoneType,
 	Email 		 EmailType,
 	primary key (CID)
@@ -60,7 +60,7 @@ create table Client (
 create table Company (
 	CID 		 serial references Client(CID),
 	URL 		 URLType,
-	ABN  		 text,
+	ABN  		 integer(11),
 	primary key (CID)
 );
 
@@ -73,4 +73,68 @@ create domain CarLicenseType as
 create domain OptionType as varchar(12)
 	check (value in ('sunroof','moonroof','GPS','alloy wheels','leather'));
 
-create domain VINType as char(17) check ...
+create domain VINType as char(17) check (value ~ '[^IOQ]')
+
+create table Car (
+	VIN 		 VINType,
+	Manufacturer varchar(40) not null,
+	Model 		 varchar(40) not null,
+	Year		 integer not null,
+	primary key (VIN)
+);
+
+-- Options
+
+create table Options (
+	VIN 		 VINType references Car(VIN),
+	Options      OptionType,
+	primary key (VIN, Options),
+	foreign key (VIN)
+);
+
+create table NewCar (
+	VIN 		 VINType references Car(VIN),
+	Cost 		 text not null,
+	Charges  	 text not null,
+	SellsNew	 serial references Salesman(EID),
+	"Date"	 	 date,
+	Price 	  	 text,
+	Commission 	 text,
+	Client 	  	 serial references Client(CID),
+	PlateNumber  varchar(6) not null,
+	primary key (VIN),
+	foreign key (SellsNew),
+	foreign key (Client)
+);
+
+create table UsedCar (
+	VIN  		 VINType references Car(VIN),
+	PlateNumber  varchar(6) not null,
+	Buys		 serial references Salesman(EID),
+	Sells 	 	 serial references Salesman(EID),
+	"Date"	 	 date,
+	Price 	  	 text,
+	Commission 	 text,
+	Client 	  	 serial references Client(CID),
+	primary key (VIN),
+	foreign key (Buys),
+	foreign key (Sells),
+	foreign key (Client)
+);
+
+-- Repair Job
+
+create table RepairJob (
+	EID			serial references Mechanic(EID),
+	"Number"  	integer,
+	Description varchar(250),
+	Parts  		text,
+	Work 		text,
+	"Date"  	date,
+	Repairs 	VINType not null references UsedCar(VIN),
+	PaidBy 		CID not null references Client(CID),
+	primary key (EID, "Number"),
+	foreign key (EID),
+	foreign key (Repairs),
+	foreign key (PaidBy)
+);
