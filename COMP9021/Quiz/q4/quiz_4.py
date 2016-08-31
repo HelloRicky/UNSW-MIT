85267b28570e2b28c6b8ceb9958a12b289325b67
Rick Fu Zheng <rfzheng@eutility.com.au>
2016-08-31 16:58:01 +1000

    q4 completed

--- COMP9021/Quiz/q4/quiz_4.py
+++ COMP9021/Quiz/q4/quiz_4.py
@@ -19,24 +19,68 @@
 import sys
 import os
 import csv
+import re
 
 
 filename = 'monthly.csv'
+
 if not os.path.exists(filename):
     print('There is no file named {} in the working directory, giving up...'.format(filename))
     sys.exit()
 
 source = input('Enter the source (GCAG or GISTEMP): ')
+
+if source != 'GCAG' and source != 'GISTEMP':
+    print('Wrong source, giving up...')
+    sys.exit()
+
 range_for_the_years = input('Enter a range for the years in the form XXXX--XXXX: ')
+if not re.match(r'^[0-9]{4}--[0-9]{4}$', range_for_the_years):
+    print('Wrong format, giving up...')
+    sys.exit()
+
 month = input('Enter a month in the form of a 2-digit number: ')
+if not re.match(r'^[0-9]{2}$', month):
+    print('Wrong format, giving up...')
+    sys.exit()
+
 average = 0
+data_avg = []
+data_year_fit = []
 years_above_average = []
 
 # REPLACE THIS COMMENT WITH YOUR CODE
 
+src_key = 'Source'
+mean_key = 'Mean'
+date_key = 'Date'
+date1 = int(range_for_the_years[:4])
+date2 = int(range_for_the_years[6:])
+startDate = min(date1, date2)
+endDate = max(date1, date2)
+
 with open(filename) as csvfile:
-    pass
-    # REPLACE pass ABOVE AND THIS COMMENT WITH YOUR CODE
+    reader = csv.DictReader(csvfile)
+    
+    for row in reader:
+        #check if source match up
+        if row[src_key] == source:
+            #check for year
+            year = int(row[date_key][:4])
+            if year >= startDate and year <= endDate:
+                # check for month
+                mth = row[date_key][-2:]
+                if mth == month:
+                    val = float(row[mean_key])
+                    data_avg.append(val)
+                    data_year_fit.append([val, year])
+                    
+if len(data_avg):
+    average = sum(data_avg)/len(data_avg)
+    for i in data_year_fit:
+        if i[0] > average:
+            years_above_average.append(i[1])
+    years_above_average.sort()
 
 print('The average anomaly for this month of those years is: {:.2f}.'.format(average))
 print('The list of years when the temperature anomaly was above average is:')
