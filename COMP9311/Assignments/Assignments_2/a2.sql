b6cc8dfe6ec7b664c4b2ab184b3c4b40c8efef74
Rick Fu Zheng <rfzheng@eutility.com.au>
2016-09-16 15:28:11 +1000

    a2 question 11 done

--- COMP9311/Assignments/Assignments_2/a2.sql
+++ COMP9311/Assignments/Assignments_2/a2.sql
@@ -15,7 +15,7 @@ create or replace view Q2(Code) as
 	SELECT Code
 	FROM Executive
 	GROUP BY Code
-	HAVING COUNT(Person) > 5;
+	HAVING COUNT(Person) >= 6;
 ;
 -- Q3
 
@@ -29,15 +29,18 @@ create or replace view Q3(Name) as
 create or replace view Q4(Sector, Number) as
 	SELECT Sector, COUNT(Industry)
 	FROM Category
-	GROUP BY Sector;
+	GROUP BY Sector
+	ORDER BY Sector
+	;
 ;
 
--- Q5!!!
+-- Q5!!! do I need to filter Name only??
 
 create or replace view Q5(Name) as ...
 	SELECT e.Person
 	FROM Q3, Executive e, Company c 
-	WHERE c.Name = Q3.Name and c.Code = e.Code;
+	WHERE c.Name = Q3.Name and c.Code = e.Code
+	ORDER BY e.Person;
 
 
 -- Q6
@@ -48,35 +51,74 @@ create or replace view Q6(Name) as ...
 	WHERE cat.Code = c.Code and cat.Sector = 'Services' 
 	and c.Country = 'Australia' and c.Zip LIKE '2%';
 
--- Q7!!!
 
-create or replace view Q7("Date", Code, Volume, PrevPrice, Price, Change, Gain) as ...
+-- Q7
 
-	SELECT MAX("Date"), Code
+create or replace view Q7_1("Date", Code, Volume, Price, PrevPrice) as
+	SELECT "Date", Code, Volume, Price, 
+	lag(Price, 1) over (partition by Code ORDER BY "Date")
+	FROM ASX;
+;
+
+
+create or replace view Q7("Date", Code, Volume, PrevPrice, Price, Change, Gain) as 
+
+	SELECT "Date", Code, Volume, PrevPrice, Price,
+	(Price - PrevPrice), (Price - PrevPrice)*100
+	FROM Q7_1
+	where "Date" <> (SELECT MIN("Date") FROM Q7_1)
+	;
+;
+	
+
+
+-- Q8
+
+create or replace view Q8_1("Date", Volume) as
+	SELECT "Date", Max(Volume)
 	FROM ASX
-	GROUP BY Code;
+	GROUP BY "Date";
+;
 
--- Q8!!!
+create or replace view Q8("Date", Code, Volume) as
 
-create or replace view Q8("Date", Code, Volume) as ...
-
-	SELECT "Date"
-	FROM ASX
-	GROUP BY "Date"
-	ORDER BY "Date" and Code;
-
+	SELECT ASX."Date", ASX.Code, ASX.Volume
+	FROM ASX, Q8_1
+	WHERE ASX.Volume = Q8_1.Volume AND ASX."Date" = Q8_1."Date"
+	ORDER BY ASX."Date", ASX.Code
+	;
+;
 -- Q9
 
-create or replace view Q9(Sector, Industry, Number) as ...
+create or replace view Q9_1(Industry, Number) as
+	SELECT Industry, count(Code)
+	FROM Category
+	GROUP BY Industry;
+;
+
+create or replace view Q9(Sector, Industry, Number) as
+	SELECT DISTINCT cat.Sector, cat.Industry, Q9_1.Number
+	FROM Q9_1 LEFT JOIN Category cat ON (Q9_1.Industry = cat.Industry)
+	ORDER BY cat.Sector, cat.Industry;
+;
 
 -- Q10
 
-create or replace view Q10(Code, Industry) as ...
-
+create or replace view Q10(Code, Industry) as 
+	SELECT cat.Code, cat.Industry
+	FROM Q9, Category cat
+	WHERE Q9.Number = 1 and Q9.Industry = cat.Industry;
+;
 -- Q11
 
-create or replace view Q11(Sector, AvgRating) as ...
+create or replace view Q11(Sector, AvgRating) as
 
+	SELECT cat.Sector, AVG(r.Star) as result
+	FROM Category cat, Rating r 
+	WHERE cat.Code = r.Code
+	GROUP BY cat.Sector
+	ORDER BY result DESC;
+;
 -- Q12
 
 create or replace view Q12(Name) as ...
