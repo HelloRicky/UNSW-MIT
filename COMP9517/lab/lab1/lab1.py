023fb603a7a99294c85f922c9cc3f7d0db59973a
HelloRicky <ricky.zheng2@gmail.com>
2018-08-01 22:42:09 +1000

    modify combine method

--- COMP9517/lab/lab1/lab1.py
+++ COMP9517/lab/lab1/lab1.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 
 
+def crop_img_position(x1, x2):
+  ## find out diff
+  if x1 < x2:
+    return 0, (x2-x1)/2 
+  return (x1-x2)/2, 0
 
 def combineImages(img1, img2, imgOut, axis = 1):
   ## axis = 1 for stacking vertically, 0 for horizontally
@@ -9,17 +14,26 @@ def combineImages(img1, img2, imgOut, axis = 1):
   ## arg2 = [1, 0, -1], where 1 = color, 0 = grayscale, -1 = unchanged
   _img1 = cv2.imread(img1, 0)
   _img2 = cv2.imread(img2, 0)
-  _imgOut = np.concatenate((_img1, _img2), axis = axis)
+
+  h1, w1 = _img1.shape
+  h2, w2 = _img2.shape
+
+  c_h1, c_h2 = crop_img_position(h1, h2)
+  c_w1, c_w2 = crop_img_position(w1, w2)
+
+  crop_img1 = _img1[c_h1:h1-c_h1, c_w1:w1/2]
+  crop_img2 = _img2[c_h2:h2-c_h2, w2/2:w2-c_w2]
+
+  _imgOut = np.concatenate((crop_img1, crop_img2), axis = axis)
   cv2.imwrite(imgOut, _imgOut)
 
-def 
 
 if __name__ == "__main__":
-  img1 = 'DataSamples/1.png'
+  img1 = 'DataSamples/6.png'
   img2 = 'DataSamples/2.png'
   imgOut = 'image3.png'
 
   ## Q2
-  combineImages(img1, img2, imgOut, axis = 0)
+  combineImages(img1, img2, imgOut)
 
   ## Q3
