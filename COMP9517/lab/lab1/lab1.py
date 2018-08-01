10a988f5d7e6da12be9fdb7c70743c446f40a40c
Ricky <ricky.zheng2@gmail.com>
2018-08-01 17:25:44 +1000

    add lab1 files

--- COMP9517/lab/lab1/lab1.py
+++ COMP9517/lab/lab1/lab1.py
@@ -5,14 +5,21 @@ import cv2
 
 def combineImages(img1, img2, imgOut, axis = 1):
   ## axis = 1 for stacking vertically, 0 for horizontally
+  ## imread(arg1, arg2)
+  ## arg2 = [1, 0, -1], where 1 = color, 0 = grayscale, -1 = unchanged
   _img1 = cv2.imread(img1, 0)
   _img2 = cv2.imread(img2, 0)
   _imgOut = np.concatenate((_img1, _img2), axis = axis)
   cv2.imwrite(imgOut, _imgOut)
 
+def 
+
 if __name__ == "__main__":
   img1 = 'DataSamples/1.png'
   img2 = 'DataSamples/2.png'
-  imgOut = 'out.png'
-  
+  imgOut = 'image3.png'
+
+  ## Q2
   combineImages(img1, img2, imgOut, axis = 0)
+
+  ## Q3
