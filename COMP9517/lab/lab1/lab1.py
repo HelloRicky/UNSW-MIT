f629fa06c12222d3f953da73829a8c24481410c3
HelloRicky <ricky.zheng2@gmail.com>
2018-08-02 01:15:59 +1000

    finish q2-q5

--- COMP9517/lab/lab1/lab1.py
+++ COMP9517/lab/lab1/lab1.py
@@ -12,28 +12,58 @@ def combineImages(img1, img2, imgOut, axis = 1):
   ## axis = 1 for stacking vertically, 0 for horizontally
   ## imread(arg1, arg2)
   ## arg2 = [1, 0, -1], where 1 = color, 0 = grayscale, -1 = unchanged
-  _img1 = cv2.imread(img1, 0)
-  _img2 = cv2.imread(img2, 0)
 
-  h1, w1 = _img1.shape
-  h2, w2 = _img2.shape
+  h1, w1 = img1.shape
+  h2, w2 = img2.shape
 
   c_h1, c_h2 = crop_img_position(h1, h2)
   c_w1, c_w2 = crop_img_position(w1, w2)
 
-  crop_img1 = _img1[c_h1:h1-c_h1, c_w1:w1/2]
-  crop_img2 = _img2[c_h2:h2-c_h2, w2/2:w2-c_w2]
+  crop_img1 = img1[c_h1:h1-c_h1, c_w1:w1/2]
+  crop_img2 = img2[c_h2:h2-c_h2, w2/2:w2-c_w2]
 
   _imgOut = np.concatenate((crop_img1, crop_img2), axis = axis)
   cv2.imwrite(imgOut, _imgOut)
 
 
+def quantization(data, scale, imgOut):
+  print(np.max(data))
+  new_data = data/255*scale
+  new_data = np.round(new_data)
+  new_data = new_data*(255/scale)
+  print(new_data[0,0])
+  return cv2.imwrite(imgOut, new_data)
+
+def image_enhancement(data, imgOut):
+  L = np.max(data)
+  new_data = L - 1 - data
+  return cv2.imwrite(imgOut, new_data)
+
+def contrast_stretching(data, imgOut):
+  a = 0
+  b = 255
+  c = np.min(data)
+  d = np.max(data)
+  new_data = (data - c)*((b-a)/(d-c)) + a
+  return cv2.imwrite(imgOut, new_data)
+
+
 if __name__ == "__main__":
-  img1 = 'DataSamples/6.png'
-  img2 = 'DataSamples/2.png'
+  img_file_1 = 'DataSamples/8.png'
+  img_file_2 = 'DataSamples/6.png'
   imgOut = 'image3.png'
 
+  img1 = cv2.imread(img_file_1, 0)
+  img2 = cv2.imread(img_file_2, 0)
   ## Q2
-  combineImages(img1, img2, imgOut)
+  #combineImages(img1, img2, imgOut)
 
   ## Q3
+  quantization(img1, 15, imgOut)
+  
+  ## Q4
+  #image_enhancement(img1, imgOut)
+
+  ## Q5
+  #contrast_stretching(img1, imgOut)
+  ## Q6
