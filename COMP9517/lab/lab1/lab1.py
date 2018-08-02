84aab562de5d93b8faf7e8c663e2b94de6fee1f4
Ricky <ricky.zheng2@gmail.com>
2018-08-02 10:41:11 +1000

    q6 added

--- COMP9517/lab/lab1/lab1.py
+++ COMP9517/lab/lab1/lab1.py
@@ -1,17 +1,28 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+import os.path
 
+
+## helper functions
+##------------------------------------------------------------------------
 
 def crop_img_position(x1, x2):
-  ## find out diff
+  """
+  find out diff value for different dimension
+  return 0 if equal
+  """
+  
   if x1 < x2:
     return 0, (x2-x1)/2 
   return (x1-x2)/2, 0
 
 def combineImages(img1, img2, imgOut, axis = 1):
-  ## axis = 1 for stacking vertically, 0 for horizontally
-  ## imread(arg1, arg2)
-  ## arg2 = [1, 0, -1], where 1 = color, 0 = grayscale, -1 = unchanged
+  """
+  axis = 1 for stacking vertically, 0 for horizontally
+  imread(arg1, arg2)
+  arg2 = [1, 0, -1], where 1 = color, 0 = grayscale, -1 = unchanged
+  """
 
   h1, w1 = img1.shape
   h2, w2 = img2.shape
@@ -26,7 +37,7 @@ def combineImages(img1, img2, imgOut, axis = 1):
   cv2.imwrite(imgOut, _imgOut)
 
 
-def quantization(data, level, imgOut):
+def quantization(data, imgOut, level):
   b = 2**level - 1
   new_data = np.round(data/255*b)
   new_data = new_data*(255/b)
@@ -45,24 +56,36 @@ def contrast_stretching(data, imgOut):
   new_data = (data - c)*((b-a)/(d-c)) + a
   return cv2.imwrite(imgOut, new_data)
 
-def histogram_grey_level(data, imgOut):
-  L = np.max(data)
-  h = np.apply_along_axis(lambda x: np.bincount(x), axis=1, arr=data)
-  print(h)
+def histogram_grey_level(data):
+
+  ## plot graph in line
+  plt.figure(1)
+  plt.subplot(211)
+  plt.imshow(data, cmap='gray')
+ 
+
+  ## convert to 1d array
+  plt.subplot(212)
+  plt.hist(data.flatten())
+
+  plt.show()
+
 
 if __name__ == "__main__":
-  img_file_1 = 'DataSamples/8.png'
+  img_file_1 = 'DataSamples/1.png'
   img_file_2 = 'DataSamples/6.png'
+  
   imgOut = 'image3.png'
 
   img1 = cv2.imread(img_file_1, 0)
-  img2 = cv2.imread(img_file_2, 0)
+  img2 = cv2.imread(img_file_2, 0) 
+  
   ## Q2
   #combineImages(img1, img2, imgOut)
 
   ## Q3
   ## level = [5,3,1]
-  #quantization(img1, level=5, imgOut)
+  quantization(img1, imgOut, level=3)
   
   ## Q4
   #image_enhancement(img1, imgOut)
@@ -70,6 +93,6 @@ if __name__ == "__main__":
   ## Q5
   #contrast_stretching(img1, imgOut)
 
-
   ## Q6
-  histogram_grey_level(img1, imgOut)
+  img3 = cv2.imread(imgOut, 0) if os.path.exists(imgOut) else img1
+  histogram_grey_level(img3)
